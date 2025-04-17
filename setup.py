77b9f31789deed1efb0041a38d083a397728b9da
tomerbr3456 <59730830+tomerbr3456@users.noreply.github.com>
2025-04-17 13:17:31 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="microservice-robust-storage",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.1",
+        "pandas>=1.2.0",
+        "numpy>=1.20.0",
+    ],
+    author="",
+    author_email="",
+    description="Microservice service implementing NumPy architecture",
+    keywords="microservice-robust-storage, python",
+    url="",
+)
